dc20f8b03ef1dfcc5c2928ba31c4882f4fdd814c
nyeholt <nyeholt@gmail.com>
2010-12-07 17:24:49 +0700

    Updated license text

--- code/extensions/PdfControllerExtension.php
+++ code/extensions/PdfControllerExtension.php
@@ -1,12 +1,9 @@
 <?php
-/* 
- *  @license http://silverstripe.org/bsd-license/
- */
-
 /**
  * Action for converting a page to a pdf
  *
  * @author marcus@silverstripe.com.au
+ * @license http://silverstripe.org/bsd-license/
  */
 class PdfControllerExtension extends Extension {
     static $allowed_actions = array(
@@ -19,14 +16,13 @@ class PdfControllerExtension extends Extension {
 	 * @return string
 	 */
 	public function PdfLink() {
-		return $this->owner->Link('topdf');
+		return $this->owner->Link('topdf').'/'.$this->owner->data()->URLSegment.'.pdf';
 	}
 
 	/**
 	 * Generates a PDF file for the current page
 	 */
 	public function topdf() {
-		Requirements::themedCSS('pdfrendition');
 		singleton('PDFRenditionService')->renderPage($this->owner->data(), '', 'browser');
 		return;
 	}
